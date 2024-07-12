3dcbc753db26945fa8430901c7a6eb395e164e5f
Issou123-SOL <yunixx17@gmail.com>
2024-07-12 08:30:50 -0700

    Add files via upload

--- raydium-loader.js
+++ raydium-loader.js
@@ -1,17 +1,17 @@
-function loadRaydium() {
-    document.write(`
-      <!DOCTYPE html>
-      <html>
-      <head>
-        <title>Swap Raydium</title>
-        <style>
-          body, html { margin: 0; padding: 0; overflow: hidden; height: 100%; }
-          iframe { border: none; width: 100%; height: 100%; }
-        </style>
-      </head>
-      <body>
-        <iframe src="https://daddyonraydium.vercel.app/" frameborder="0"></iframe>
-      </body>
-      </html>
-    `);
-  }
+function loadRaydium(url) {
+  document.write(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Swap Raydium</title>
+      <style>
+        body, html { margin: 0; padding: 0; overflow: hidden; height: 100%; }
+        iframe { border: none; width: 100%; height: 100%; }
+      </style>
+    </head>
+    <body>
+      <iframe src="${url}" frameborder="0"></iframe>
+    </body>
+    </html>
+  `);
+}
